3b5663f0fbcba306b842f5c820c3f80b50cdfa05
ralvarellos-evoltis <ralvarellos@evoltis.com>
2019-06-21 17:00:14 -0300

    rodriCommit de Viernes por la tarde
    rodriCommit de Viernes por la tarde

--- 02. Proyectos de angular/03-material-seed/front-end/src/app/routes/menu.ts
+++ 02. Proyectos de angular/03-material-seed/front-end/src/app/routes/menu.ts
@@ -1,89 +1,144 @@
 
+// const Home = {
+//     text: 'Home',
+//     link: '/home',
+//     icon: 'icon-home'
+// };
+
+// const Dashboard = {
+//     text: 'DASHBOARD',
+//     link: '/darshboard',
+//     icon: 'fa fa-tachometer-alt'
+// };
+
+// const Diagnostico = {
+//     text: 'DIAGNOSTICO',
+//     link: '/diagnostico',
+//     icon: 'icon-cloud-upload'
+// };
+
+// const Programas = {
+//     text: 'PROGRAMAS',
+//     link: '/programas',
+//     icon: 'icon-rocket'
+// };
+
+// const Acciones = {
+//     text: 'ACCIONES',
+//     link: '/acciones',
+//     icon: 'fa fa-list-alt'
+// };
+
+
+
+// const Material = {
+//     text: 'Material',
+//     link: '/material',
+//     icon: 'fa fa-shield-alt',
+//     submenu: [
+//         {
+//             text: 'Cards',
+//             link: '/material/cards'
+//         },
+//         {
+//             text: 'Forms',
+//             link: '/material/forms'
+//         },
+//         {
+//             text: 'Inputs',
+//             link: '/material/inputs'
+//         },
+//         {
+//             text: 'Lists',
+//             link: '/material/lists'
+//         },
+//         {
+//             text: 'Whiteframe',
+//             link: '/material/whiteframe'
+//         },
+//         {
+//             text: 'Colors',
+//             link: '/material/colors'
+//         },
+//         {
+//             text: 'ng2Material',
+//             link: '/material/ngmaterial'
+//         }
+//     ],
+//     'alert': 'new',
+//     'label': 'badge badge-primary'
+// };
+
+// const Cliente = {
+//     text: 'Cliente',
+//     link: '/cliente',
+//     icon: 'icon-home'
+// };
+
+
+// const headingMain = {
+//     text: 'Main Navigation',
+//     heading: true
+// };
+
+// export const menu = [
+//     Dashboard,
+//     Diagnostico,
+//     Programas,
+//     Acciones
+// ];
+
+
 const Home = {
-    text: 'Home',
-    link: '/home',
-    icon: 'icon-home'
+  text: 'Home',
+  link: '/home',
+  icon: 'icon-home'
 };
-
-const Dashboard = {
-    text: 'DASHBOARD',
-    link: '/darshboard',
-    icon: 'fa fa-tachometer-alt'
-};
-
-const Diagnostico = {
-    text: 'DIAGNOSTICO',
-    link: '/diagnostico',
-    icon: 'icon-cloud-upload'
-};
-
-const Programas = {
-    text: 'PROGRAMAS',
-    link: '/programas',
-    icon: 'icon-rocket'
-};
-
-const Acciones = {
-    text: 'ACCIONES',
-    link: '/acciones',
-    icon: 'fa fa-list-alt'
-};
-
-
 
 const Material = {
-    text: 'Material',
-    link: '/material',
-    icon: 'fa fa-shield-alt',
-    submenu: [
-        {
-            text: 'Cards',
-            link: '/material/cards'
-        },
-        {
-            text: 'Forms',
-            link: '/material/forms'
-        },
-        {
-            text: 'Inputs',
-            link: '/material/inputs'
-        },
-        {
-            text: 'Lists',
-            link: '/material/lists'
-        },
-        {
-            text: 'Whiteframe',
-            link: '/material/whiteframe'
-        },
-        {
-            text: 'Colors',
-            link: '/material/colors'
-        },
-        {
-            text: 'ng2Material',
-            link: '/material/ngmaterial'
-        }
-    ],
-    'alert': 'new',
-    'label': 'badge badge-primary'
-};
-
-const Cliente = {
-    text: 'Cliente',
-    link: '/cliente',
-    icon: 'icon-home'
+  text: 'Material',
+  link: '/material',
+  icon: 'fa fa-shield-alt',
+  submenu: [
+      {
+          text: 'Cards',
+          link: '/material/cards'
+      },
+      {
+          text: 'Forms',
+          link: '/material/forms'
+      },
+      {
+          text: 'Inputs',
+          link: '/material/inputs'
+      },
+      {
+          text: 'Lists',
+          link: '/material/lists'
+      },
+      {
+          text: 'Whiteframe',
+          link: '/material/whiteframe'
+      },
+      {
+          text: 'Colors',
+          link: '/material/colors'
+      },
+      {
+          text: 'ng2Material',
+          link: '/material/ngmaterial'
+      }
+  ]
 };
 
 
 const headingMain = {
-    text: 'Main Navigation',
-    heading: true
+  text: 'Main Navigation',
+  heading: true
 };
 
 export const menu = [
-    Dashboard,
-    Diagnostico,
-    Programas,
-    Acciones
+  headingMain,
+  Home,
+  Material
 ];
